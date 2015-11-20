f9c1677ca31d406765f13849c83a28d3bc9a7534
Luigi Poole <luigipoole@outlook.com>
2015-11-19 22:42:07 -0800

    add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+
+    });
+
+    // Actually load this plugin's task(s).
+    grunt.loadTasks('tasks');
+};
