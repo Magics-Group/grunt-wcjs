5400a2b5333e9768fbcb88fa4f9267887194d98f
Luigi Poole <luigipoole@outlook.com>
2015-11-19 22:42:35 -0800

    Add defualts and downloader libs

---
+++ tasks/lib/defualts.js
@@ -0,0 +1,9 @@
+module.exports = {
+    RUNTIME_VERSION: 'latest',
+    RUNTIME_ARCH: process.arch,
+    RUNTIME_PLATFORM: process.platform,
+    RUNTIME: 'electron',
+    WCJS_VERSION: 'latest',
+    FORCE: false,
+    DIR: 'WebChimera'
+};
